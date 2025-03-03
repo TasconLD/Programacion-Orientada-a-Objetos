8895f412eb81beee2667218acd77913c930aeda9
TasconLD <166366956+TasconLD@users.noreply.github.com>
2025-03-03 18:40:07 -0500

    Update Marzo 03.cpp

--- Marzo/Marzo 03.cpp
+++ Marzo/Marzo 03.cpp
@@ -4,4 +4,11 @@ clase es una plantilla de un objeto
 clase: carro
 objeto: kia, renault (instancias de clase)
 
+1. identificar objetos del mundo real
+2. identificar clases y sus atributos
+3. identificar los metodos de cada clase
+4. desarrollar un diagrama de clases
+5. implementar la solucion siguiento el diagrama de clases
+6. realizar pruebas a los metodos de clase
+
 */
