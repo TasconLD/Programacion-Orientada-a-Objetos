ddaa78d3707df2849adf0ea71de978621d19bddb
TasconLD <166366956+TasconLD@users.noreply.github.com>
2025-03-31 18:44:47 -0500

    Update Marzo31.cpp

--- Marzo/Marzo31.cpp
+++ Marzo/Marzo31.cpp
@@ -4,7 +4,12 @@ lenguaje c++
 que son los archivos .h: estructura o defincion del objeto, atributos y metodos, .cpp: metodos del codigo, main: llamar los objetos
 que es abstracion, encapsulamiento, polimorfismo, herencia
 atributos privados y metodos publicos
+para que clase hija herede atributos deben estar en protect y no en privado, y hereda tanto atributos como metodos
 
 
+_________________________________________________
+-Referencias:
+int valor=5;
+int &meses = valor; // meses es un nuevo nombre para valor, es una referencia, todo lo que le pasa a valor le pasa a meses
 
 */
