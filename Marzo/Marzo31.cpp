47df85eb1a2624a9a999130cac5e4cc959ae2289
TasconLD <166366956+TasconLD@users.noreply.github.com>
2025-03-31 18:23:43 -0500

    Update Marzo31.cpp

--- Marzo/Marzo31.cpp
+++ Marzo/Marzo31.cpp
@@ -1,3 +1,10 @@
-//despues de semana santa parcial 1
+/*despues de semana santa parcial 1
+temas:
+lenguaje c++
+que son los archivos .h: estructura o defincion del objeto, atributos y metodos, .cpp: metodos del codigo, main: llamar los objetos
+que es abstracion, encapsulamiento, polimorfismo, herencia
+atributos privados y metodos publicos
 
 
+
+*/
