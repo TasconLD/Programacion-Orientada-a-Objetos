5c293fc274533c8fcbfb06e5ba3bedb77b54b42e
TasconLD <166366956+TasconLD@users.noreply.github.com>
2025-04-15 17:56:06 -0500

    Update BloodDataBase.h

--- Abril/Laboratorio 2/BloodDataBase.h
+++ Abril/Laboratorio 2/BloodDataBase.h
@@ -1,28 +1,31 @@
 #ifndef BLOODDATABASE_H
 #define BLOODDATABASE_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
 #include "Donor.h"
-#include <string>
-#include <iostream>
 
 class BloodDatabase {
 private:
-    // Vector que almacena los donantes
+    const std::string fileName = "data.txt";
     std::vector<Donor> donors;
-    
-public:
-    // Métodos públicos para interactuar con la base de datos de donantes
-    void getDonorDetails(); // Método para ingresar los detalles del donante
-    void writeDataToFile(); // Método para guardar los datos en un archivo
-    void searchAndDisplay() const; // Método para buscar y mostrar detalles de un donante
-    void deleteDonor(const std::string& donorName); // Método para eliminar un donante
-    void showAllDonors() const; // Método para mostrar todos los donantes registrados
 
-    // Métodos estáticos
-    static void clearConsole(); // Método para limpiar la pantalla de la consola
-    static void waitForKeyPress(); // Método para esperar que el usuario presione una tecla
+    static void displayProvinces();
+
+public:
+    static void clearConsole();
+    static void waitForKeyPress();
+    static int getValidatedInput(const std::string& prompt);
+
+    void getDonorDetails();
+    void writeDataToFile();
+    void searchAndDisplay() const;
+    void deleteDonor(const std::string& donorName);
+
+    // Nuevo método para mostrar todos los donantes registrados
+    void showAllDonors() const;
 };
 
 #endif // BLOODDATABASE_H
-
