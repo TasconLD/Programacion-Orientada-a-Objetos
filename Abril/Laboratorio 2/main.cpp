03f1335a35bc8fc703cb0092f71c9a7290dc83be
TasconLD <166366956+TasconLD@users.noreply.github.com>
2025-04-15 22:20:10 -0500

    Update main.cpp

--- Abril/Laboratorio 2/main.cpp
+++ Abril/Laboratorio 2/main.cpp
@@ -6,56 +6,55 @@
 using namespace std;
 
 int main() {
-    string donorName;
+    std::string donorName;
     BloodDatabase database;
     int choice;
 
     while (true) {
         BloodDatabase::clearConsole();
 
-        cout << " ░█████╗░██████╗░██╗░░░██╗███████╗  ██████╗░░█████╗░░░░░░██╗░█████╗░\n"
-             " ██╔══██╗██╔══██╗██║░░░██║╚════██║  ██╔══██╗██╔══██╗░░░░░██║██╔══██╗\n"
-             " ██║░░╚═╝██████╔╝██║░░░██║░░███╔═╝  ██████╔╝██║░░██║░░░░░██║███████║\n"
-             " ██║░░██╗██╔══██╗██║░░░██║██╔══╝░░  ██╔══██╗██║░░██║██╗░░██║██╔══██║\n"
-             " ╚█████╔╝██║░░██║╚██████╔╝███████╗  ██║░░██║╚█████╔╝╚█████╔╝██║░░██║\n"
-             " ░╚════╝░╚═╝░░╚═╝░╚═════╝░╚══════╝  ╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝\n";
+        std::cout <<
+            " ░█████╗░██████╗░██╗░░░██╗███████╗  ██████╗░░█████╗░░░░░░██╗░█████╗░\n"
+            " ██╔══██╗██╔══██╗██║░░░██║╚════██║  ██╔══██╗██╔══██╗░░░░░██║██╔══██╗\n"
+            " ██║░░╚═╝██████╔╝██║░░░██║░░███╔═╝  ██████╔╝██║░░██║░░░░░██║███████║\n"
+            " ██║░░██╗██╔══██╗██║░░░██║██╔══╝░░  ██╔══██╗██║░░██║██╗░░██║██╔══██║\n"
+            " ╚█████╔╝██║░░██║╚██████╔╝███████╗  ██║░░██║╚█████╔╝╚█████╔╝██║░░██║\n"
+            " ░╚════╝░╚═╝░░╚═╝░╚═════╝░╚══════╝  ╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝\n";
 
-        cout << "1. Registrar donante\n";
-        cout << "2. Buscar donante\n";
-        cout << "3. Eliminar donante\n";
-        cout << "4. Ver todos los donantes\n";  // Opción para ver todos los donantes
-        cout << "5. Salir\n";
-        cout << "Ingrese su elección: ";
-        cin >> choice;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpiar el buffer
+        std::cout << "1. Registrar donante\n";
+        std::cout << "2. Buscar donante\n";
+        std::cout << "3. Eliminar donante\n";
+        std::cout << "4. Salir\n";
+        std::cout << "5. Ver todos los donantes\n";  // Nueva opción para ver todos los donantes
+        std::cout << "Ingrese su elección: ";
+        std::cin >> choice;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // descartar cualquier entrada extra
 
         switch (choice) {
-            case 1:
-                database.getDonorDetails();
-                database.writeDataToFile();
-                break;
-            case 2:
-                database.searchAndDisplay();
-                break;
-            case 3:
-                cout << "Ingrese el nombre del donante a eliminar: ";
-                getline(cin, donorName);
-                database.deleteDonor(donorName);
-                BloodDatabase::waitForKeyPress();
-                break;
-            case 4:
-                database.showAllDonors(); // Llamar a la nueva función para mostrar todos los donantes
-                break;
-            case 5:
-                cout << "Gracias por usar el Sistema de la Cruz Roja" << endl;
-                return 0;
-            default:
-                cout << "Opción no válida. Inténtalo de nuevo.\n";
-                BloodDatabase::waitForKeyPress();
-                break;
+        case 1:
+            database.getDonorDetails();
+            database.writeDataToFile();
+            break;
+        case 2:
+            database.searchAndDisplay();
+            break;
+        case 3:
+            std::cout << "Ingrese el nombre del donante a eliminar: ";
+            std::getline(std::cin, donorName);
+            database.deleteDonor(donorName);
+            BloodDatabase::waitForKeyPress();
+            break;
+        case 4:
+            std::cout << "Gracias por usar el Sistema de la Cruz Roja" << std::endl;
+            return 0;
+        case 5:
+            database.displayAllDonors(); // Llamada al método para mostrar todos los donantes
+            break;
+        default:
+            std::cout << "Opción no válida. Inténtalo de nuevo.\n";
+            BloodDatabase::waitForKeyPress();
+            break;
         }
     }
-
-    return 0;
 }
 
