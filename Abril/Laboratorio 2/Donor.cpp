7c70ddcf271370641ea41cb8535d65e0b4dd04b3
TasconLD <166366956+TasconLD@users.noreply.github.com>
2025-04-15 23:17:11 -0500

    Update Donor.cpp

--- Abril/Laboratorio 2/Donor.cpp
+++ Abril/Laboratorio 2/Donor.cpp
@@ -1,8 +1,8 @@
 #include "Donor.h"
 
 // Constructor que inicializa todos los atributos de un donante
-Donor::Donor(string name, string bloodType, string id, string phone)
-    : name(name), bloodType(bloodType), id(id), phone(phone) {}
+Donor::Donor(string name, string bloodType, string id, string phone, string city)
+    : name(name), bloodType(bloodType), id(id), phone(phone), city(city) {}
 
 // Métodos que devuelven cada uno de los datos del donante
 
@@ -13,6 +13,19 @@ string Donor::getName() const {
 string Donor::getBloodType() const {
     return bloodType;
 }
+
+string Donor::getId() const {
+    return id;
+}
+
+string Donor::getPhone() const {
+    return phone;
+}
+
+string Donor::getCity() const {
+    return city;
+}
+
 
 string Donor::getId() const {
     return id;
