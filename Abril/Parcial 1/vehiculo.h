12f56d623a8b0249d4e1721a074009834e9b30b8
TasconLD <166366956+TasconLD@users.noreply.github.com>
2025-04-21 20:03:41 -0500

    Update vehiculo.h

--- Abril/Parcial 1/vehiculo.h
+++ Abril/Parcial 1/vehiculo.h
@@ -1,16 +1,20 @@
+
+
 #ifndef VEHICULO_H
 #define VEHICULO_H
 
 #include <string>
-#include "Sangre.h"
+#include "Auto.h"
 using namespace std;
 
 //Clase del donante
 class Vehiculo {
 private:
+    Auto tipoAuto;
     string placa;
     string marca;
     string color;
+    string hora;
     
 
 public:
@@ -19,7 +23,7 @@ public:
     Vehiculo();
 
     Vehiculo(const string& marca, const string& marca,
-            const string& color);
+            const string& color, const string& hora, const Auto& tipoAuto );
 
     // Getters y setters
     string getPlaca() const;
@@ -31,13 +35,20 @@ public:
     string getColor() const;
     void setColor(const string& color);
 
+    string getHora() const;
+    void setHora(const string& hora);
+
+    string getTipoAuto() const;
+    void setTipoAuto(const Auto& tipoAuto);
 
 
      //Validaciones de datos utilizando expresiones regulares
     static bool validarPlaca(const string& placa);
     static bool validarMarca(const string& marca);
     static bool validarColor(const string& color);
+    static bool validarHora(const string& hora);
+    
     
 };
 
-#endif // DONANTE_H
+#endif // VEHICULO_H
